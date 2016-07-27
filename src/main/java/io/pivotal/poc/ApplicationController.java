003977c7362244248fd31bf2db816694f018c635
Alex Shumilov <ashumilov@pivotal.io>
2016-07-27 14:34:26 -0400

    cleanup and add request params for mq properties

--- src/main/java/io/pivotal/poc/ApplicationController.java
+++ src/main/java/io/pivotal/poc/ApplicationController.java
@@ -1,42 +1,17 @@
 package io.pivotal.poc;
 
-//import io.pivotal.pcfs.ibmmq.mock.MQQueueConnectionFactory;
-import com.ibm.mq.jms.*;
-import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.ApplicationContextAware;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import com.ibm.msg.client.wmq.WMQConstants;
+import org.springframework.web.bind.annotation.RequestParam;
 
-import javax.jms.JMSException;
-import javax.jms.Session;
-import javax.jms.TextMessage;
-import java.util.Arrays;
-import java.util.List;
 import java.util.logging.Logger;
-
-import static com.ibm.disthub2.impl.formats.Envelop.Constants.TextMessage;
 
 @Controller
 @RequestMapping("/")
-public class ApplicationController implements ApplicationContextAware {
-
-    @Autowired
-    private ApplicationContext applicationContext;
-
-    @Autowired
-    private BagOfProperties bagOfProperties;
-
-//    @Autowired
-//    private MQQueueConnectionFactory mqQueueConnectionFactory;
-
-//    @Autowired
-//    private MessageService messageService;
+public class ApplicationController {
 
     @Autowired
     private MQSender mqSender;
@@ -44,50 +19,19 @@ public class ApplicationController implements ApplicationContextAware {
 
     private static Logger logger = Logger.getLogger(ApplicationController.class.getName());
 
-
-
-
-    @Override
-    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
-        this.applicationContext = applicationContext;
-    }
-
     @RequestMapping(value = "/send", method = RequestMethod.GET)
-    public String receveMessage(ModelMap model) throws Exception {
+    public String receveMessage(ModelMap model
+            , @RequestParam String host
+            , @RequestParam int port
+            , @RequestParam String queueManager
+            , @RequestParam String channel
+            , @RequestParam String queue
+    ) throws Exception {
+        logger.info("********" + host);
         logger.info("***Sending to queue");
-        String receivedMessage = mqSender.pingQueue();
+        String receivedMessage = mqSender.pingQueue(host, port, queueManager, channel, queue);
         logger.info("***Queue responded to queue");
         model.addAttribute("msgArgument", "Sent and Received message to jms, check logs. " + receivedMessage);
-        return "index";
-    }
-
-//    @RequestMapping(value = "/props", method = RequestMethod.GET)
-//    public String props(ModelMap model) {
-//
-//        model.addAttribute("bag", mqQueueConnectionFactory);
-//
-//        return "bag-of-properties";
-//    }
-
-    @RequestMapping(value = "/", method = RequestMethod.GET)
-    public String hello(ModelMap model) {
-
-
-        String[] beanNames = applicationContext.getBeanDefinitionNames();
-        List<String> list = Arrays.asList(beanNames);
-
-        //this bean is of time MQQueueConnectionFactory
-        Object bean = applicationContext.getBean("mq-service");
-
-        model.addAttribute("beans", list);
-
-        return "beans";
-    }
-
-    @RequestMapping(value = "/Print/{arg}", method = RequestMethod.GET)
-    public String welcomeName(@PathVariable String arg, ModelMap model) {
-        model.addAttribute("msgArgument", "Maven Java Web Application Project, input variable: " + arg);
-
         return "index";
     }
 }
--- src/main/java/io/pivotal/poc/MQSender.java
+++ src/main/java/io/pivotal/poc/MQSender.java
@@ -20,12 +20,12 @@ public class MQSender {
     @Value("${mq.queue:FAPI.CL.QUEUE}")
     private String queue;
 
-    /**
-     *
-     * @return echo message sent to queue
-     * @throws Exception
-     */
-    public String pingQueue() throws Exception {
+
+    public String pingQueue(String host
+            , int port
+            , String queueManager
+            , String channel
+            , String queue) throws Exception {
         MQEnvironment.hostname = host;
         MQEnvironment.port = port;
         MQEnvironment.channel = channel;
@@ -34,12 +34,13 @@ public class MQSender {
         String nl = System.getProperty("line.separator");
 
         logger.info("****Sending message" + nl
-        + " Hostname: " + host + nl
-        + " Port: " + port + nl
-        + " Channel: " + channel + nl
-        + " Queuemanager: " + queuemanager);
+                + " Hostname: " + host + nl
+                + " Port: " + port + nl
+                + " Channel: " + channel + nl
+                + " Queuemanager: " + queueManager + nl
+                + " Queue: " + queue);
 
-        MQQueueManager qMgr = new MQQueueManager(queuemanager);
+        MQQueueManager qMgr = new MQQueueManager(queueManager);
         logger.info(qMgr.toString());
 
         int openOptions = CMQC.MQOO_BROWSE | CMQC.MQOO_INQUIRE | CMQC.MQOO_OUTPUT | CMQC.MQOO_INPUT_AS_Q_DEF;
@@ -48,7 +49,9 @@ public class MQSender {
         logger.info("Queue size:" + destQueue.getCurrentDepth());
         MQMessage mqMessage = new MQMessage();
         logger.info("MQMessage message created");
-        mqMessage.writeUTF("Sending Sample message");
+        String now = String.valueOf(System.currentTimeMillis());
+
+        mqMessage.writeUTF("Sending Sample message --> " + now);
         MQPutMessageOptions pmo = new MQPutMessageOptions();
 
         destQueue.put(mqMessage, pmo);
@@ -61,6 +64,15 @@ public class MQSender {
         destQueue.close();
         qMgr.disconnect();
         return receivedMessage;
+    }
+
+
+    /**
+     * @return echo message sent to queue
+     * @throws Exception
+     */
+    public String pingQueue() throws Exception {
+        return pingQueue(this.host, this.port, this.queuemanager, this.channel, this.queue);
     }
 
     private static Logger logger = Logger.getLogger(MQSender.class.getName());
